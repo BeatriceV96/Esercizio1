40145d294078b3614438063d4407690465db0af0
BeatriceV96 <164747965+BeatriceV96@users.noreply.github.com>
2024-04-12 16:18:16 +0200

    Update D10.js

--- Progetto_settimanale 3/D10/D10.js
+++ Progetto_settimanale 3/D10/D10.js
@@ -12,7 +12,7 @@ REGOLE
 /* ESERCIZIO A
   Crea una variabile chiamata "sum" e assegnaci il risultato della somma tra i valori 10 e 20.
 */
-let sum = 10 + 30;
+let sum = 10 + 20;
 
 /* ESERCIZIO B
   Crea una variabile chiamata "random" e assegnaci un numero casuale tra 0 e 20 (deve essere generato dinamicamente a ogni esecuzione).
